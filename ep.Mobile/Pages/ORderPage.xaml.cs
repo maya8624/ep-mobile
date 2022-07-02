e3cfe0d615f9509861b9913d50297ddd6be98999
Andy Jung <Andy.Jung@framegroup.com.au>
2022-07-02 19:12:26 +1000

    Comment out CommunityToolkit.Extensions

--- ep.Mobile/Pages/ORderPage.xaml.cs
+++ ep.Mobile/Pages/ORderPage.xaml.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ep.Mobile.Interfaces;
-using ep.Mobile.Models;
-using ep.Mobile.PageModels;
-using Xamarin.CommunityToolkit.Extensions;
+//using Xamarin.CommunityToolkit.Extensions;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -20,10 +13,10 @@ namespace ep.Mobile.Pages
             InitializeComponent();
         }        
       
-        private async void Button_Clicked(object sender, EventArgs e)
-        {
-            var result = await Navigation.ShowPopupAsync(new QRPopupPage());
-            await DisplayAlert("Result", $"Result:{result}", "OK");
-        }
+        //private async void Button_Clicked(object sender, EventArgs e)
+        //{
+        //    var result = await Navigation.ShowPopupAsync(new QRPopupPage());
+        //    await DisplayAlert("Result", $"Result:{result}", "OK");
+        //}
     }
 }
--- ep.Mobile/Pages/QRPopupPage.xaml.cs
+++ ep.Mobile/Pages/QRPopupPage.xaml.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.CommunityToolkit.UI.Views;
+//using Xamarin.CommunityToolkit.UI.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace ep.Mobile.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class QRPopupPage : Popup
+    public partial class QRPopupPage// : Popup
     {
         public QRPopupPage()
         {
