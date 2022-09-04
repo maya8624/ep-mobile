aec97a86e00f11f5b54b4f950c0fd679c1efd688
Andy Jung <Andy.Jung@framegroup.com.au>
2022-09-04 12:30:20 +1000

    Change the app icon

--- ep.Android/MainActivity.cs
+++ ep.Android/MainActivity.cs
@@ -6,7 +6,7 @@ using ep.Mobile;
 
 namespace epMobile.Droid
 {
-    [Activity(Label = "ep.Mobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
